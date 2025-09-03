0eeb796a123dd14dee0c2f541a0ac67b5183523f
kodekloudsiva <kodekloudsiva@gmail.com>
2025-09-03 19:46:54 +0530

    testyu

--- cicd/scripts/migration-entrypoint.sh
+++ cicd/scripts/migration-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+echo "migration entry point, pwd: " && pwd
+
+echo "migration entry point, ls: " && ls -lrta
+
 echo "📦 Ensuring database [$DB_NAME] exists..."
 /opt/mssql-tools/bin/sqlcmd -S $DB_HOST -U $DB_USER -P $DB_PASS -d master -Q "IF DB_ID('$DB_NAME') IS NULL CREATE DATABASE [$DB_NAME];"
 
